1129ecff8f7ea566ef29567ad477fa0fa540ad6c
hiuphn <phamhuynhngochieu468@gmail.com>
2024-05-21 21:53:52 +0700

    fix subcategory, done forgot password

--- WebBanHang/Models/Subcategory.cs
+++ WebBanHang/Models/Subcategory.cs
@@ -1,28 +1,17 @@
 ﻿using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel;
-using System.ComponentModel.DataAnnotations;
 
 namespace WebBanHang.Models
 {
     public class Subcategory
     {
         
-            public int Id { get; set; }
 
-            [Required]
-            public string Name { get; set; }
+        public int Id { get; set; }
+        public string Name { get; set; }
+     
 
-            public string Description { get; set; }
-
-            [Required]
-            [ForeignKey("Category")]
-            public int CategoryId { get; set; }
-
-            public Category Category { get; set; }
-        }
-
-
-
+       
     }
 
-
+}
