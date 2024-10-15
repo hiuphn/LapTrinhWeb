6b5fecc7a9ad26eea89278ede202b1d22804e86d
hiuphn <145784093+hiuphn@users.noreply.github.com>
2024-10-15 20:36:24 +0700

    Delete Tuan3 directory

--- Tuan3/Models/Grade.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Tuan3.Models
-{
-    public class Grade
-    {
-        public int GradeId { get; set; }
-        public string GradeName { get; set;}
-        public IList<Student> Students { get; set; }
-    }
-}
